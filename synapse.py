589e495ece50f2c5c66ec25c15a9002257ad5b37
Terry Stewart <tcstewar@uwaterloo.ca>
2014-09-10 15:10:49 -0400

    Rewrote synapse models

--- synapse.py
+++ synapse.py
@@ -1,56 +1,83 @@
 import numpy as np
 
 class Synapse(object):
-    def __init__(self, n_synapses, bits=16):
-        self.value = np.zeros(n_synapses, dtype='i32')
-        self.bits = bits
-        if bits == 16:
-            self.value_bit_mask = 0xFFFF
-        else:
-            self.value_bit_mask = 0xFFFF - ((1 << (16-bits))-1)
-
-    def limit_bits(self):
-        # ensures only self.bits bits are stored in self.value
-        self.value &= self.value_bit_mask
-
-class SynapseStandard(Synapse):
-    def __init__(self, n_synapses, bits=16, dt=0.001, tau=0.008):
-        Synapse.__init__(self, n_synapses=n_synapses, bits=bits)
+    def __init__(self, n_synapses, dt):
+        self.n_synapses = n_synapses
         self.dt = dt
+    def step(self):
+        raise NotImplementedError()
+
+    def fixed_rate_response(self, rate, weight=1.0, T=1.0):
+        result = []
+        spike_count = 0
+        for i in range(int(T/self.dt)):
+            if spike_count < rate * i * self.dt:
+                spike_count += 1
+                input = weight
+            else:
+                input = 0
+            result.append(list(self.step(input)))
+        return result, np.mean(result[len(result)/2:])
+
+class ExpStandard(Synapse):
+    def __init__(self, n_synapses, dt=0.001, tau=0.016):
+        super(ExpStandard, self).__init__(n_synapses=n_synapses, dt=dt)
+        self.current = np.zeros(n_synapses, dtype=float)
         self.decay = np.exp(-dt/tau)
     def step(self, spikes):
-        J = np.asarray(spikes << 16, dtype='i32')
-        self.value *= self.decay
-        self.value += J * (1 - self.decay)
-        self.limit_bits()
-        return self.value
+        self.current *= self.decay
+        self.current += (1 - self.decay) * spikes
+        return self.current
 
-class SynapseQuick(Synapse):
-    def __init__(self, n_synapses, bits=16, dt=0.001, tau=0.008):
-        Synapse.__init__(self, n_synapses=n_synapses, bits=bits)
-        self.dt = dt
+class ExpFixed(Synapse):
+    def __init__(self, n_synapses, dt=0.001, tau=0.016, bits=20):
+        super(ExpFixed, self).__init__(n_synapses=n_synapses, dt=dt)
+        self.current = np.zeros(n_synapses, dtype='i32')
         self.decay_shift = 0
         while (1 << self.decay_shift) * dt < tau:
             self.decay_shift += 1
-    def step(self, spikes):
-        J = np.asarray(spikes , dtype='i32')
 
-        decay = self.value >> self.decay_shift
-        self.value -= decay
-        # if we're rounding our decay to 0, jump to 0
-        #self.value[decay==0] = 0
-        self.value += (J << self.bits)
-
-        shift = self.decay_shift - 16 + self.bits
-        if shift < 0:
-            return self.value << -shift
+        self.bits = bits
+        if bits == 20:
+            self.current_bit_mask = 0xFFFFF
         else:
-            return self.value >> shift
+            self.current_bit_mask = 0xFFFFF - ((1 << (20-bits))-1)
 
-        return self.value >> (self.decay_shift - 16 + self.bits)
+    def step(self, spikes):
+        J = np.asarray((spikes * 0x10000), dtype='i32') >> self.decay_shift
+
+        decay = self.current >> self.decay_shift
+        self.current -= decay
+        # if we're rounding our decay to 0, jump to 0
+        self.current[decay==0] = 0
+        self.current += J
+
+        self.current &= self.current_bit_mask
+
+        return self.current.astype('float') / 0x10000
+
 
 
 if __name__ == '__main__':
+    import pylab
+    tau = 0.016
+    rate = 300
+    weight = 8.0
+    syn = ExpStandard(n_synapses=1, tau=tau)
+    response, avg = syn.fixed_rate_response(rate, weight=weight)
+    pylab.plot(response,label='std: %1.3f' % avg)
+
+    syn = ExpFixed(n_synapses=1, tau=tau, bits=10)
+    response, avg = syn.fixed_rate_response(rate, weight=weight)
+    pylab.plot(response,label='fixed: %1.3f' % avg)
+
+    pylab.legend()
+    pylab.show()
+    1/0
+
+
+
+
     bits = 16
     tau = 0.128
     syn1 = SynapseStandard(1, tau=tau, bits=bits)
